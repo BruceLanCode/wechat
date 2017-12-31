8bc124037f2b1e58d74670a3a7d545a7fec85546
lantuhui <shengxiaobai15@163.com>
2017-12-31 23:07:27 +0800

    cxt.req is different from cxt.request
    cxt.req is different from cxt.request

--- practice01/middleware/validate.js
+++ practice01/middleware/validate.js
@@ -1,85 +1,14 @@
 'use strict'
 
 const sha1 = require('sha1');
-const Promise = require('bluebird');
-const request = Promise.promisify(require('request'));
+const getRawBody = require('raw-body');
+const contentType = require('content-type');
+const Wechat = require('./wechat');
 
-const prefix = 'https://api.weixin.qq.com/cgi-bin/token';
-const api = {
-    accessToken: prefix + '?grant_type=client_credential'
-}
-
-class Wechat {
-    constructor(props) {
-        this.appID = props.appID;
-        this.appSecret = props.appsecret;
-        this.getAccessToken = props.getAccessToken;
-        this.saveAccessToken = props.saveAccessToken;
-
-        this.getAccessToken()
-            .then(data => {
-                try {
-                    data = JSON.parse(data);
-                }
-                catch(e) {
-                    // console.log(e);
-                    return this.updateAccessToken()
-                }
-
-                if(this.isValidAccessToken(data)) {
-                    // console.log('Promise:',Promise);
-                    Promise.resolve(data);
-                } else {
-                    return this.updateAccessToken()
-                }
-            })
-            .then(data => {
-                this.access_token = data.access_token;
-                this.expires_in = data.expires_in;
-
-                this.saveAccessToken(data);
-            })
-    }
-
-    isValidAccessToken(data) {
-        if(!data || !data.access_token || !data.expires_in) {
-            return false;
-        }
-
-        let expires_in = data.expires_in;
-        let now = (new Date().getTime());
-
-        if(now < expires_in) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    updateAccessToken() {
-        const appID = this.appID;
-        const appSecret = this.appSecret;
-        const url = api.accessToken + '&appid=' + appID + '&secret=' + appSecret;
-
-        return new Promise((resolve,reject) => {
-            request({
-                url,
-                json: true
-            }).then(res => {
-                let data = res.body;
-                let now = (new Date().getTime());
-                let expires_in = now + (data.expires_in - 20) * 1000;
-
-                data.expires_in = expires_in;
-
-                resolve(data);
-            })
-        });
-    }
-}
 
 module.exports = config =>{
     let wechat = new Wechat(config.wechat);
+
     return async (ctx,next) => {
         console.log(ctx.query);
 
@@ -91,14 +20,27 @@ module.exports = config =>{
 
         let str = [token, timestamp, nonce].sort().join('');
         let sha = sha1(str);
-        console.log(sha);
-        console.log(signature);
-        console.log(echostr);
 
-        if(sha === signature) {
-            ctx.body = echostr;
-        } else {
-            ctx.body = 'wrong'
+        if(ctx.method === 'GET') {
+            if(sha === signature) {
+                ctx.body = echostr;
+            } else {
+                ctx.body = 'wrong'
+            }
+        } else if (ctx.method === 'POST') {
+            if(sha !== signature) {
+                ctx.body = 'wrong';
+                return false;
+            }
+
+            // console.log(ctx.request);
+            let data = await getRawBody(ctx.req, {
+                length: ctx.request.headers['content-length'],
+                limit: '1mb',
+                encoding: contentType.parse(ctx.req).parameters.charset
+            });
+
+            console.log(data);
         }
     }
 };
---
+++ practice01/middleware/wechat.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const Promise = require('bluebird');
+const request = Promise.promisify(require('request'));
+
+const prefix = 'https://api.weixin.qq.com/cgi-bin/token';
+const api = {
+    accessToken: prefix + '?grant_type=client_credential'
+}
+
+class Wechat {
+    constructor(props) {
+        this.appID = props.appID;
+        this.appSecret = props.appsecret;
+        this.getAccessToken = props.getAccessToken;
+        this.saveAccessToken = props.saveAccessToken;
+
+        this.getAccessToken()
+            .then(data => {
+                try {
+                    data = JSON.parse(data);
+                }
+                catch(e) {
+                    // console.log(e);
+                    return this.updateAccessToken()
+                }
+
+                if(this.isValidAccessToken(data)) {
+                    return Promise.resolve(data);
+                } else {
+                    return this.updateAccessToken()
+                }
+            })
+            .then(data => {
+                this.access_token = data.access_token;
+                this.expires_in = data.expires_in;
+
+                this.saveAccessToken(data);
+            })
+    }
+
+    isValidAccessToken(data) {
+        if(!data || !data.access_token || !data.expires_in) {
+            return false;
+        }
+
+        let expires_in = data.expires_in;
+        let now = (new Date().getTime());
+
+        if(now < expires_in) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    updateAccessToken() {
+        const appID = this.appID;
+        const appSecret = this.appSecret;
+        const url = api.accessToken + '&appid=' + appID + '&secret=' + appSecret;
+
+        return new Promise((resolve,reject) => {
+            request({
+                url,
+                json: true
+            }).then(res => {
+                let data = res.body;
+                let now = (new Date().getTime());
+                let expires_in = now + (data.expires_in - 20) * 1000;
+
+                data.expires_in = expires_in;
+
+                resolve(data);
+            }).catch(err => {
+                console.log(err)
+            })
+        });
+    }
+}
+
+module.exports = Wechat;
